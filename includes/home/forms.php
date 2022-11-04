fdbcf17e5dfe3358127eff6c8060f32a7530be84
Renan <orefusado@gmail.com>
2022-11-04 04:53:08 -0300

    melhorias html e css

--- includes/home/forms.php
+++ includes/home/forms.php
@@ -1,45 +1,45 @@
 <?php
 
 use \App\Upload;
+$multFiles = true;
 
 echo "<div class='sendfiles'>";
 
-// SISTMEA PARA FORMULÁRIO DE UM ARQUIVO
-include __DIR__ . '/form.html';
+if ($multFiles) {
+    // SISTEMA PARA FORMULÁRIO DE VÁRIOS ARQUIVOS
+    include __DIR__ . '/multi-form.html';
 
-if (isset($_FILES['sentFile'])) {
-    $uploadObj = new Upload($_FILES['sentFile']);
+    if (isset($_FILES['sentFiles'])) {
+        
+        $uploads = Upload::createMultipleUploads($_FILES['sentFiles']);
 
-    $uploadObj->generateRandomName();
+        if ($uploads) {
+            foreach($uploads as $uploadObj) {
+                $success = $uploadObj->upload('files', false);
 
-    $success = $uploadObj->upload(__DIR__.'/files', false);
-    if ($success) {
-        echo "Arquivo <b>" . $uploadObj->getBasename() . "</b> enviado com sucesso!";
-    } else {
-        echo "Não foi possível enviar o arquivo";
-    }
-}
-
-// SISTEMA PARA FORMULÁRIO DE VÁRIOS ARQUIVOS
-include __DIR__ . '/multi-form.html';
-
-if (isset($_FILES['sentFiles'])) {
-    
-    $uploads = Upload::createMultipleUploads($_FILES['sentFiles']);
-
-    if ($uploads) {
-        foreach($uploads as $uploadObj) {
-            $uploadObj->generateRandomName();
-            $success = $uploadObj->upload(__DIR__.'/files', false);
-
-            if ($success) {
-                echo "Arquivo <b>" . $uploadObj->getBasename() . "</b> enviado com sucesso! <br>";
-            } else {
-                echo "Não foi possível enviar o arquivo <br>";
+                if ($success) {
+                    echo "Arquivo <b>" . $uploadObj->getBasename() . "</b> enviado com sucesso! <br>";
+                } else {
+                    echo "Não foi possível enviar o arquivo <br>";
+                }
             }
+        } else {
+            echo "Não foi possível enviar o arquivo <br>";
         }
-    } else {
-        echo "Não foi possível enviar o arquivo <br>";
+    }
+} else {
+    // SISTMEA PARA FORMULÁRIO DE UM ARQUIVO
+    include __DIR__ . '/form.html';
+
+    if (isset($_FILES['sentFile'])) {
+        $uploadObj = new Upload($_FILES['sentFile']);
+
+        $success = $uploadObj->upload('files', false);
+        if ($success) {
+            echo "Arquivo <b>" . $uploadObj->getBasename() . "</b> enviado com sucesso!";
+        } else {
+            echo "Não foi possível enviar o arquivo";
+        }
     }
 }
 echo "</div>";
